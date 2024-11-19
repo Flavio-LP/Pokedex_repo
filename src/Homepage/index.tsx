374920426a85cb0218b111aedcccab61e1e04135
Flavio Pirola <flavio.pirola@cristalcopo.com.br>
2024-11-19 13:34:21 -0300

    Início do projeto

---
+++ src/Homepage/index.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+function Homepage(){
+
+    return(
+        <div>
+            <div className='Side_bar'>
+                <h1>teste</h1>
+            </div>
+        </div>
+    )
+
+};
+
+export default Homepage
