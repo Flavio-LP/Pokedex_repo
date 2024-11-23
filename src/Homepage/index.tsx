702cded2d51befbce7bd71fca1fdacb1cb73f27d
Flavio-LP <flavio5070@outlook.com>
2024-11-23 18:48:49 -0300

    Updated Homepage, inserted topbar, sidebar and adjusted the page body

--- src/Homepage/index.tsx
+++ src/Homepage/index.tsx
@@ -1,13 +1,23 @@
-
 import React from 'react'
+import './index.css';
 
 function Homepage(){
 
     return(
-        <div>
-            <div className='Side_bar'>
-                <h1>teste</h1>
+        <div className='Homepage'>
+
+            <div className='Top_bar'>
+
+                <h1 className='Top_bar_title'>Pokédex</h1>
+
             </div>
+
+            <div className='Side_bar'>
+                <h3 className='Side_bar_title'>Search a pokémon</h3>
+
+                <input className='Side_bar_input'></input>
+            </div>
+
         </div>
     )
 
