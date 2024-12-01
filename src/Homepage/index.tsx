84c38cddd2e6bbdef4819af1457cf9a7c6d1d498
Flavio-LP <flavio5070@outlook.com>
2024-12-01 17:48:55 -0300

    added axios and created request

--- src/Homepage/index.tsx
+++ src/Homepage/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import {useState} from 'react'
 import './index.css';
+import axios from 'axios';
 
 function Homepage(){
 
+
+    const [pokemon, setPokemon] = useState("");
+    // const [pokemonData, setPokemonData] = useState([])
+    // const [pokemonType, setPokemonType] = useState("")
+
+
+    const getPokemon = async() => {
+        const toArray = [];
+        try{
+            const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+            const res = await axios.get(url)
+            toArray.push(res.data);
+         
+        } catch(e) {
+            console.log(e)
+        }
+    } 
+
+
+
+     const handleChange = (e: { target: { value: string; }; }) => {
+        getPokemon(); 
+        //setPokemon(e.target.value.toLowerCase())
+     }
+
+    
     return(
+
+        
 
         <div className='Homepage'>
 
@@ -13,11 +42,34 @@ function Homepage(){
 
             </div>
 
-            <div className='Side_bar'>
-                <h3 className='Side_bar_title'>Search a pokémon</h3>
+            <div className='body_page'> 
+                <div className='Side_bar'>
+                    <h3 className='Side_bar_title'>Search a pokémon</h3>
 
-                <input className='Side_bar_input'></input>
-            </div>
+                    <input className='Side_bar_input' onChange={handleChange} ></input>
+
+                    <div className='stats_pokemon'>
+                        <h2>HP</h2>
+                        <p className='hp_stats'></p>
+                        <h2>Attack</h2>
+                        <p className='attack_stats'></p>
+                        <h2>Defense</h2>
+                        <p className='defense_stats'></p>
+                        <h2>Speed</h2>
+                        <p className='speed_stats'></p>
+                        <h2>Types</h2>
+                        <p className='types_stats'></p>
+                    </div>
+                </div>
+
+                <div className='Center_div'>
+                    <h1 >{pokemon}</h1>
+                </div>
+
+            </div>    
+
+
+
 
 
         </div>
